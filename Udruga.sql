33baa17229b15a8aab72cf4a7f3a1b794a3c71f9
Lanaa <108399001+LanaJelenic@users.noreply.github.com>
2023-05-09 21:08:36 +0200

    Update Udruga.sql

--- Udruga.sql
+++ Udruga.sql
@@ -1,31 +1,22 @@
 create database UDRUGA;
 use UDRUGA
-create table OSOBA(
-ID_OSOBA int NOT NULL PRIMARY KEY,
-IME varchar(50),
-PREZIME varchar(50),
-ID_ZIVOTINJE int FOREIGN KEY REFERENCES ZIVOTINJA(ID_ZIVOTINJE)
+create table osoba(
+osobaID int not null primary key identity(1,1),
+ime varchar(50),
+prezime varchar(50),
+sticenikID int foreign key references sticenik_udruge(sticenikID)
 );
 
-create table ZIVOTINJA(
-ID_ZIVOTINJE int NOT NULL PRIMARY KEY,
-VRSTA varchar(100),
-TEŽINA decimal,
-NJEGA varchar(200)
+create table sticenik_udruge (
+sticenikID int not null primary key identity(1,1),
+vrsta varchar(50),
+tezina decimal(18,2),
+boja varchar(50),
+prostorID int foreign key references prostor(prostorID)
 );
 
-create table PROSTOR(
-ID_PROSTORA int NOT NULL PRIMARY KEY,
-SIRINA int,
-DULJINA decimal,
-ID_ZIVOTINJE int FOREIGN KEY REFERENCES ZIVOTINJA(ID_ZIVOTINJE)
-);
-
-create table UDRUGA(
-ID_UDRUGA int NOT NULL PRIMARY KEY,
-NAZIV varchar(50),
-ID_OSOBA int FOREIGN KEY REFERENCES OSOBA(ID_OSOBA),
-ID_ZIVOTINJE int FOREIGN KEY REFERENCES ZIVOTINJA(ID_ZIVOTINJE),
-ID_PROSTORA int FOREIGN KEY REFERENCES PROSTOR(ID_PROSTORA),
-ADRESA varchar(100)
+create table prostor(
+prostorID int not null primary key identity (1,1),
+sirina decimal(18,2),
+visina decimal(18,2)
 );
