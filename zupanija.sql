99b58ee4fcfc83a7e5d6fcb1ae4df4212feb55e1
Lanaa <108399001+LanaJelenic@users.noreply.github.com>
2023-05-16 17:08:29 +0200

    Update zupanija.sql

--- zupanija.sql
+++ zupanija.sql
@@ -25,3 +25,27 @@ mjestoID int not null primary key identity(1,1),
 opcinaID int foreign key references opcina(opcinaID),
 naziv varchar(50)
 );
+
+insert into zupan(ime,prezime)
+values('Ivan','Anušić'),
+('Damir','Dekanić'),
+('Ivan','Celjak');
+
+select*from zupan;
+
+insert into zupanija(naziv,zupanID)
+values('Osiječko-baranjska',1),
+('Vukovarsko-srijemska',2),
+('Sisačko-moslavačka',3);
+
+select*from zupanija;
+
+insert into opcina(naziv,zupanijaID)
+values('Bilje',1),
+('Čeminac',1),
+('Ivankovo',2),
+('Borovo',2),
+('Dvor',3),
+('Topusko',3);
+
+select*from opcina;
