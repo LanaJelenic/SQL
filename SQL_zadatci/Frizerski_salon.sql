de2c24668c90893c36aee31404d817fc43bd53ee
Lanaa <108399001+LanaJelenic@users.noreply.github.com>
2023-05-18 18:24:44 +0200

    Update Frizerski_salon.sql

--- SQL_zadatci/Frizerski_salon.sql
+++ SQL_zadatci/Frizerski_salon.sql
@@ -28,3 +28,39 @@ djelatnikID int foreign key references djelatnik(djelatnikID),
 korisnikID int foreign key references korisnik(korisnikID),
 uslugaID int foreign key references usluga(uslugaID)
 );
+
+insert into djelatnik(ime,prezime)
+values
+('Ana','Anić'),
+('Mila','Barišić'),
+('Antonela','Ivić'),
+('Iva','Milković');
+
+select*from djelatnik;
+
+insert into korisnik(ime,prezime,spol,duzina_kose)
+values
+('Anja','Sarić','žensko',13),
+('Marko','Josipović','muško',5),
+('Ema','Antunović','žensko',10),
+('Josip','Ćupić','muško',3);
+
+select*from korisnik;
+
+insert into usluga(cijena,naziv)
+values
+(9.50,'šišanje'),
+(5.00,'šišanje'),
+(14.30,'bojanje'),
+(10.00,'blajhanje');
+
+select*from usluga;
+
+insert into termin(datum,djelatnikID,korisnikID,uslugaID)
+values
+('2023-03-16 15:30:00',4,2,2),
+('2023-03-17 10:00:00',1,1,1),
+('2023-03-18 19:20:00',2,3,3),
+('2023-03-19 13:00:00',3,4,4);
+
+select*from termin;
