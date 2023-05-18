baef444020d3f677a4eef5a4b6384b43b99c81eb
Lanaa <108399001+LanaJelenic@users.noreply.github.com>
2023-05-18 18:37:21 +0200

    Update Salon_za_uljepsavanje.sql

--- SQL_zadatci/Salon_za_uljepsavanje.sql
+++ SQL_zadatci/Salon_za_uljepsavanje.sql
@@ -29,3 +29,41 @@ djelatnikID int foreign key references djelatnik(djelatnikID),
 korisnikID int foreign key references korisnik(korisnikID),
 uslugaID int foreign key references usluga(uslugaID)
 );
+
+insert into djelatnik(ime,prezime)
+values
+('Ana','Anić'),
+('Mila','Barišić'),
+('Antonela','Ivić'),
+('Iva','Milković');
+
+select*from djelatnik;
+
+insert into korisnik(ime,prezime,spol,boja_noktiju,oblik_noktiju)
+values
+('Marija','Jelenić','žensko','crvena','ovalni'),
+('Marina','Žuljević','žensko','žuta','badem'),
+('Stefani','Rukavina','žensko','crna','balerina'),
+('Paula','Orešković','žensko','plava','ovalni');
+
+select*from korisnik;
+
+insert into usluga(cijena,naziv)
+values
+(9.50,'rezanje'),
+(5.00,'turpijanje'),
+(14.30,'geliranje'),
+(10.00,'bojanje');
+
+select*from usluga;
+
+insert into termin(datum,djelatnikID,korisnikID,uslugaID)
+values
+('2023-04-20 15:30:00',4,4,4),
+('2023-04-21 10:00:00',1,1,1),
+('2023-04-22 19:20:00',3,3,3),
+('2023-04-23 13:00:00',2,2,2);
+
+select*from termin;
+
+
