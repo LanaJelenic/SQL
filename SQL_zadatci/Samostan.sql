964d1d9bd144febeb49e50d7969d1d2e3dd866d2
Lanaa <108399001+LanaJelenic@users.noreply.github.com>
2023-05-21 00:35:27 +0200

    Update Samostan.sql

--- SQL_zadatci/Samostan.sql
+++ SQL_zadatci/Samostan.sql
@@ -1,32 +1,64 @@
 
-create database SAMOSTAN;
-use SAMOSTAN
+create database samostan;
+use samostan
 
-create table SVECENIK(
-ID_SVECENIKA int NOT NULL PRIMARY KEY,
-IME varchar(50),
-SV_IME varchar(60),
-ID_POSLA int FOREIGN KEY REFERENCES POSAO(ID_POSLA),
-ID_NADREDENOG int FOREIGN KEY REFERENCES NADREDENI_SVECENIK(ID_NADREDENOG)
+create table svecenik(
+ID_svecenika int not null primary key identity(1,1),
+ime varchar(50),
+sv_ime varchar(60)
 );
 
-create table POSAO(
-ID_POSLA int NOT NULL PRIMARY KEY,
-NAZIV varchar(100),
+create table posao(
+ID_posla int not null primary key identity(1,1),
+naziv varchar(50)
 );
 
+create table svecenik_posao(
+ID_svecenika int foreign key references svecenik(ID_svecenika),
+ID_posla int foreign key references posao(ID_posla)
+);	
 
-create table NADREDENI_SVECENIK(
-ID_NADREDENOG int NOT NULL PRIMARY KEY,
-IME varchar(50),
-SV_IME varchar(60)
+create table nadredeni(
+ID_svecenika int foreign key references svecenik(ID_svecenika),
+ime varchar(50),
+svime varchar(60)
 );
 
-create table SAMOSTAN(
-ID_SAMOSTANA int NOT NULL PRIMARY KEY,
-NAZIV varchar(50),
-ADRESA varchar(100),
-ID_SVECENIKA int FOREIGN KEY REFERENCES SVECENIK(ID_SVECENIKA),
-ID_NADREDENOG int FOREIGN KEY REFERENCES NADREDENI_SVECENIK(ID_NADREDENOG)
-);
+insert into svecenik(ime,sv_ime)
+values
+('Nick','Franjo'),
+('Emil','Ivan'),
+('Ante','Luka');
 
+select*from svecenik;
+
+insert into posao(naziv)
+values
+('održavanje mise'),
+('priprema hostije'),
+('obuka ministranata'),
+('krizmanje'),
+('pogreb');
+
+select*from posao;
+
+insert into svecenik_posao(ID_svecenika,ID_posla)
+values
+(1,1),
+(1,3),
+(2,1),
+(3,2),
+(3,5),
+(2,4);
+
+select* from svecenik_posao;
+
+alter table nadredeni add ID_nadredenog int not null primary key identity(1,1);
+
+insert into nadredeni(ID_svecenika,ime,svime)
+values
+(1,'Pavao','Matej'),
+(2,'Pavao','Matej'),
+(3,'Pavao','Matej');
+
+select*from nadredeni;
