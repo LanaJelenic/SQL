607ab613e976d3879deef26fa7fb29aeedcba369
Lanaa <108399001+LanaJelenic@users.noreply.github.com>
2023-05-21 19:30:03 +0200

    Update Restoran.sql

--- SQL_zadatci/Restoran.sql
+++ SQL_zadatci/Restoran.sql
@@ -1,44 +1,61 @@
-create database restoran;
-use restoran
+create database restoran1;
+use restoran1
 
-create table Kategorija(
-ID_kategorije int NOT NULL PRIMARY KEY,
-ID_jela int FOREIGN KEY REFERENCES Jelo(ID_jela),
-Naziv varchar(50),
+create table jelo(
+ID_jela int not null primary key identity(1,1),
+ime varchar(50),
+cijena decimal(18,2)
 );
 
-create table Jelovnik(
-ID_jelovnika int NOT NULL PRIMARY KEY,
-Naziv varchar(50)
+create table kategorija(
+ID_kategorije int not null primary key identity(1,1),
+ID_jela int foreign key references jelo(ID_jela),
+naziv varchar(50)
 );
 
-create table Jelo(
-ID_jela int NOT NULL PRIMARY KEY,
-Ime varchar(50),
-Vrsta_pripreme varchar(100)
+create table pice(
+ID_pica int not null primary key identity(1,1),
+ime varchar(50),
+cijena decimal(18,2)
 );
 
-create table Pice(
-ID_pica int NOT NULL PRIMARY KEY,
-Naziv varchar(100),
-Proizvodat varchar(100)
+create table jelo_pice(
+ID_jela int foreign key references jelo(ID_jela),
+ID_pica int foreign key references pice(ID_pica)
 );
 
-create table Restoran(
-ID_restorana int NOT NULL PRIMARY KEY,
-Ime varchar(50),
-Adresa varchar(50),
-ID_jelovnika int FOREIGN KEY REFERENCES Jelovnik(ID_jelovnika)
-);
+insert into jelo(ime,cijena)
+values
+('Lignje',9.89),
+('Ćevapi',6.00),
+('Ražnjići',9.50);
 
-ALTER TABLE Jelovnik
-add ID_kategorije int FOREIGN KEY REFERENCES Kategorija(ID_kategorije);
+select*from jelo;
 
-alter table Jelo
-add ID_pica int FOREIGN KEY REFERENCES Pice(ID_pica);
+insert into kategorija(ID_jela,naziv)
+values
+(1,'Morska jela'),
+(2,'Grill'),
+(3,'Piletina');
 
-alter table Pice
-add ID_jela int FOREIGN KEY REFERENCES Jelo(ID_jela);
+select*from kategorija;
 
-alter table Restoran
-add ID_kategorije int FOREIGN KEY REFERENCES Kategorija(ID_kategorije);
+insert into pice(ime,cijena)
+values
+('Coca cola',1.99),
+('Juice',1.00),
+('Jamnica',2.00),
+('Sok od jabuke',1.50);
+
+select*from pice;
+
+insert into jelo_pice(ID_jela,ID_pica)
+values
+(1,1),
+(1,2),
+(2,2),
+(2,3),
+(3,3),
+(3,4);
+
+select*from jelo_pice;
