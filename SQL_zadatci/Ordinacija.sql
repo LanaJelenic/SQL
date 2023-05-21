8eecd2611fb40a1dec7b1c23be8157630f3e6afc
Lanaa <108399001+LanaJelenic@users.noreply.github.com>
2023-05-22 00:01:20 +0200

    Update Ordinacija.sql

--- SQL_zadatci/Ordinacija.sql
+++ SQL_zadatci/Ordinacija.sql
@@ -1,35 +1,51 @@
-create database Ordinacija;
-use Ordinacija
+create database Ordinacija1;
+use Ordinacija1
 
-create table Pacijent(
-ID_pacijenta int NOT NULL PRIMARY KEY,
-ID_lijecenja int FOREIGN KEY REFERENCES Lijecenje(ID_lijecenja),
-Ime varchar(50),
-Prezime varchar(50),
-Broj_zdravstvene int
-);
-
-create table Lijecenje(
-ID_lijecenja int NOT NULL PRIMARY KEY,
+create table lijecenje(
+ID_lijecenja int not null primary key identity(1,1),
 Naziv varchar(50),
-Cijena int
+Cijena decimal
 );
 
-create table Med_sestra(
-ID_med_sestre int NOT NULL PRIMARY KEY,
-ID_lijecenja int FOREIGN KEY REFERENCES Lijecenje(ID_lijecenja),
-ID_pacijenta int FOREIGN KEY REFERENCES Pacijent(ID_pacijenta),
-Ime varchar(50),
-Prezime varchar(50),
-Kvalifikacija bit
+create table pacijent(
+ID_pacijenta int not null primary key identity(1,1),
+ID_lijecenja int foreign key references lijecenje(ID_lijecenja),
+ime varchar(50),
+prezime varchar(20),
+zdravstvena int
 );
 
-create table Doktor(
-ID_doktora int NOT NULL PRIMARY KEY,
+create table med_sestra(
+ID_sestre int not null primary key identity(1,1),
+ID_pacijenta int foreign key references pacijent(ID_pacijenta),
 Ime varchar(50),
 Prezime varchar(50),
-Kvalifikacija bit,
-ID_pacijenta int FOREIGN KEY REFERENCES Pacijent(ID_pacijenta),
-ID_lijecenja int FOREIGN KEY REFERENCES Lijecenje(ID_lijecenja),
-ID_med_sestre int FOREIGN KEY REFERENCES Med_sestra(ID_med_sestre)
+kvalifikacija bit
 );
+
+
+insert into lijecenje(Naziv,Cijena)
+values
+('Upala grla',10.00),
+('Slomljena ruka',20.00),
+('Upala pluca',16.90);
+
+select*from lijecenje;
+
+insert into pacijent(ID_lijecenja,Ime,Prezime,zdravstvena)
+values
+(1,'Maja','Kraljik',0954645),
+(2,'Karlo','Dvojak',09732334),
+(3,'Eva','Lovrić',0957878);
+
+select*from pacijent;
+
+insert into med_sestra(ID_pacijenta,Ime,Prezime,kvalifikacija)
+values
+(1,'Anja','Majić',1),
+(2,'Mara','Stević',0),
+(3,'Branka','Roso',1);
+
+select*from med_sestra;
+
+
