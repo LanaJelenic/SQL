475c00abb6553e5c086b406d4b8f9426428b110d
Lana <lana.jelenic@gmail.com>
2023-06-15 14:06:30 +0200

    create ConsoleApp01

---
+++ csharp/ConsoleApp01/ConsoleApp02/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Unesi ime grada:");
+string ime=
+    Console.ReadLine();
+
+Console.WriteLine(ime);
+
+Console.WriteLine(args[0]);
---
+++ csharp/ConsoleApp01/ConsoleApp03/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp03
+{
+    internal class Program
+    { 
+     static void Main(string[] args)
+        {
+            Console.WriteLine(" .              +   .                .   . .     .  .\r\n                   .                    .       .     *\r\n  .       *                        . . . .  .   .  + .\r\n            \"You Are Here\"            .   .  +  . . .\r\n.                 |             .  .   .    .    . .\r\n                  |           .     .     . +.    +  .\r\n                 \\|/            .       .   . .\r\n        . .       V          .    * . . .  .  +   .\r\n           +      .           .   .      +\r\n                            .       . +  .+. .\r\n  .                      .     . + .  . .     .      .\r\n           .      .    .     . .   . . .        ! /\r\n      *             .    . .  +    .  .       - O -\r\n          .     .    .  +   . .  *  .       . / |\r\n               . + .  .  .  .. +  .\r\n.      .  .  .  *   .  *  . +..  .            *\r\n .      .   . .   .   .   . .  +   .    .            +");
+
+        }
+    
+    
+    
+    }
+
+}
