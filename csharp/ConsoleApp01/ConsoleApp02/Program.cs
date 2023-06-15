0d24360bc21cc9414004a286e23babedc126612b
Lana <lana.jelenic@gmail.com>
2023-06-15 14:20:10 +0200

    create ConsoleApp01

---
+++ csharp/ConsoleApp01/ConsoleApp02/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Unesi ime grad:");
+string ime=
+    Console.ReadLine();
+Console.WriteLine(ime);
+
+Console.WriteLine(args[0]);
