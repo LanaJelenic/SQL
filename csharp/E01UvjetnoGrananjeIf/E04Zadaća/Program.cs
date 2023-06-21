99b0d3df746160f66627e87a77fb9a05ecbc1c9b
Lana <lana.jelenic@gmail.com>
2023-06-21 14:18:01 +0200

    update

--- csharp/E01UvjetnoGrananjeIf/E04Zadaća/Program.cs
+++ csharp/E01UvjetnoGrananjeIf/E04Zadaća/Program.cs
@@ -1,4 +1,44 @@
 ﻿int i = 3;
 int b = 5;
 
+if (i > b)
+{
+    Console.WriteLine(i);
+}
+else
+{
+    Console.WriteLine(b);
 
+}
+
+if (b > i)
+{
+
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(i);
+}
+
+if (b >= b)
+{
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Ne postoji");
+}
+
+
+Console.Write("Izaberi broj izmedu 1-999:");
+int br = int.Parse(Console.ReadLine());
+
+if (br > 999)
+{
+    Console.WriteLine("ERROR!!");
+}
+else
+{
+    Console.WriteLine(br / 10);
+}
