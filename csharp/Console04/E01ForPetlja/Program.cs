f800fccc402975506c803c534fb648fa73c42d2b
Lana <lana.jelenic@gmail.com>
2023-06-25 16:51:00 +0200

    Update Program.cs

--- csharp/Console04/E01ForPetlja/Program.cs
+++ csharp/Console04/E01ForPetlja/Program.cs
@@ -1,10 +1,136 @@
-﻿Random random = new random();
-bool playAgain = true;
-int min = 1;
-int max = 100;
-int guess;
-int number;
-int tries;
+﻿Console.WriteLine("1.-----------");
+for (int i = 0; i < 10; i = i + 1)
+{
+    Console.WriteLine("Osijek");
+}
 
+Console.WriteLine("2.-----------");
+int j = 0;
+for (j = 10; j > 0; j--)
+{
+    Console.WriteLine("Edunova");
 
+}
 
+Console.WriteLine("3.-----------");
+for (int i = 0; i < 10; i = i + 2)
+{
+    Console.WriteLine("Csharp");
+
+}
+
+Console.WriteLine("4.-----------");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("5.-----------");
+for (int i = 0; i < 10; i++)
+{
+
+    Console.WriteLine(i + 1);
+}
+
+Console.WriteLine("6.-----------");
+int zbroj = 0;
+for (int i = 1; i < 10; i++)
+{
+    zbroj += i;
+}
+Console.WriteLine(zbroj);
+
+Console.WriteLine("7.-----------");
+for (int i = 1; i <= 57; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine("8.-----------");
+zbroj = 0;
+for (int i = 2; i < 18; i++)
+{
+    if (i % 2 == 0)
+    {
+        zbroj += i;
+    }
+}
+Console.WriteLine(zbroj);
+
+int[] niz = { 2, 2, 34, 54, 5, 6, 76, 7, 8, 7, 8 };
+
+Console.WriteLine("9.-----------");
+for (int i = 0; i < niz.Length; i++)
+{
+    Console.WriteLine(niz[i]);
+}
+
+Console.WriteLine("10.--------");
+Console.Write("Unesi koliko brojeva provjeravaš:");
+int brojevi = int.Parse(Console.ReadLine());
+int broj;
+int najveci = int.MinValue;
+for (int i = 0; i < brojevi; i++)
+{
+    Console.WriteLine("Unesi {0} .broj:", i + 1);
+    broj = int.Parse(Console.ReadLine());
+    if (broj > najveci)
+    {
+        najveci = broj;
+    }
+}
+Console.WriteLine("Najveci broj je {0} ", najveci);
+
+Console.WriteLine("11.--------");
+for (; ; )
+{
+
+    Console.WriteLine(new Random().NextDouble());
+    break;
+}
+Console.WriteLine("12.--------");
+for (int i = 0; i < 10; i++)
+{
+    if (i == 3)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine("13.--------");
+for (int i = 0; i < 10; i++)
+{
+    if (i == 2 || i == 5)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("14.--------");
+
+string s;
+for (int i = 1; i < 10; i++)
+{
+    for (int k = 1; k <= 10; k++)
+    {
+        s = "  " + i * k;
+        Console.Write(s[^4..]);
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("15.--------");
+for (int i = 0; i < 10; i++)
+{
+    for (int k = 0; k < 10; k++)
+    {
+
+        goto labela;
+    }
+}
+
+labela:;
