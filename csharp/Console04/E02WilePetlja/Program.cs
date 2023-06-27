3469a37ae89eec4a91827c79fab8d8fddb655706
Lana <lana.jelenic@gmail.com>
2023-06-27 18:32:43 +0200

    create

---
+++ csharp/Console04/E02WilePetlja/Program.cs
@@ -0,0 +1,100 @@
+﻿
+while (true)
+{
+    break;
+}
+
+for (int i = 0; i < 0; i++)
+{
+    Console.WriteLine("01 Nisam ušao");
+}
+
+
+
+while (false)
+{
+    Console.WriteLine("02 Nisam ušao");
+}
+
+
+int b = 0;
+
+while (b < 10)
+{
+    Console.WriteLine(++b);
+
+}
+
+int t = 2;
+b = 0;
+while (t == 2 && b < 10)
+{
+    Console.WriteLine(++b);
+}
+Console.WriteLine("*************");
+b = 0;
+while (true)
+{
+    if (b == 2)
+    {
+        b++;
+        continue;
+    }
+    if (b == 5)
+    {
+        break;
+    }
+    Console.WriteLine(b++);
+}
+
+
+
+
+
+// Napišite program koji pomoću while petlje ispisuje 
+// Svaki 3. broj između 2 i 97
+
+// 1. rješenje
+b = 2;
+while (b <= 97)
+{
+    Console.WriteLine(b);
+    b += 3;
+}
+
+// 2. rješenje
+
+b = 2;
+while (true)
+{
+    Console.WriteLine(b);
+    b += 3;
+    if (b > 97)
+    {
+        break;
+    }
+}
+
+
+
+int zbroj = 0;
+b = 0;
+while (b++ < 100)
+{
+    zbroj += b;
+}
+Console.WriteLine(zbroj);
+
+// drugi način
+zbroj = 0; b = 1;
+while (b <= 100)
+{
+    zbroj += b;
+    b++;
+}
+Console.WriteLine(zbroj);
+
+
+
+
+
---
+++ csharp/Console04/E03DoPetlja/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+do
+{
+    Console.WriteLine("01 Ušao u petlju");
+} while (false);
+
+
+/
+int b = 0;
+do
+{
+    Console.Write("Unesi prvi cijeli broj: ");
+    b = int.Parse(Console.ReadLine());
+    if (b >= 10 && b <= 20)
+    {
+        break;
+    }
+    Console.Write("Nisi unio traženi broj.");
+}
+while (true);
+
+
+
+
+int db = 0;
+do
+{
+    Console.Write("Unesi drugi cijeli broj: ");
+    db = int.Parse(Console.ReadLine());
+    if (db >= 10 && db <= 20)
+    {
+        break;
+    }
+    Console.Write("Nisi unio traženi broj.");
+}
+while (true);
+
+//Ucitane su vrijednosti varijable b i db
+
+int manji = b < db ? b : db;
+
+if (b < db)
+{
+    manji = b;
+
+}
+else
+{
+    manji = db;
+}
+
+int veci = db > b ? db : b;
+
+
+int i = manji;
+do
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+} while (++i <= veci);
+
+
