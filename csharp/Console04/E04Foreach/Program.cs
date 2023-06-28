bb708b2e83625523e7f3ae642b56d1ab6ce08d8f
Lana <lana.jelenic@gmail.com>
2023-06-28 20:08:02 +0200

    create

---
+++ csharp/Console04/E04Foreach/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] niz = { 2, 3, 3, 4, 5, 5 };
+
+for (int i = 0; i < niz.Length; i++)
+{
+    Console.WriteLine(niz[i]);
+}
+Console.WriteLine("*******************");
+
+for (int i = niz.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(niz[i]);
+}
+Console.WriteLine("*******************");
+
+
+foreach (int en in niz)
+{
+    Console.WriteLine(en);
+}
