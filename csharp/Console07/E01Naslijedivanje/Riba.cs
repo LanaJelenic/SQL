7fa2a35c74593d3cdf98696c2b7e7b8fd31ea4e7
Lana <lana.jelenic@gmail.com>
2023-07-10 20:46:00 +0200

    update

---
+++ csharp/Console07/E01Naslijedivanje/Riba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak
+{
+    internal class Riba:Zivotinja
+    {
+        public string izgledRepa { get; set; }
+
+    }
+}
---
+++ csharp/Console07/E01Naslijedivanje/Zivotinja.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak
+{
+    internal class Zivotinja
+    {
+        public string latNaziv { get; set; }
+    }
+}
