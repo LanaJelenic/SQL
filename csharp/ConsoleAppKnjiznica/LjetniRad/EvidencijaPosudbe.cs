de527a24aa77ee91754a7d78dd22eca052edda10
Lana <lana.jelenic@gmail.com>
2023-08-09 13:14:17 +0200

    create ConsoleAppKnjiznica

---
+++ csharp/ConsoleAppKnjiznica/LjetniRad/EvidencijaPosudbe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LjetniRad
+{
+    internal class EvidencijaPosudbe:ID
+    {
+        public DateTime DatumPosudbe { get; set; }
+        public DateTime DatumVracanja { get; set; }
+        public List<Clan> Clanovi { get; set; }
+        public List<Knjiga> Knjige { get; set; }
+    }
+}
---
+++ csharp/ConsoleAppKnjiznica/LjetniRad/Knjiga.cs
@@ -0,0 +1,17 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LjetniRad
+{
+    internal class Knjiga:ID
+    {
+        public string Naslov { get; set; }
+        public string ImeAutora { get; set; }
+        public string PrezimeAutora { get; set; }
+        public string Sazetak { get; set; }
+        public int BrojStranica { get; set; }
+    }
+}
