80a3506ec0ee6c4aa15d84a4890a3038e2c63c84
Lana <lana.jelenic@gmail.com>
2023-09-01 15:04:50 +0200

    Knjiznica web api

---
+++ csharp/ConsoleAppKnjiznica/KnjiznicaWebApp/Models/ID.cs
@@ -0,0 +1,8 @@
+﻿namespace KnjiznicaWebApp.Models
+{
+    public abstract class ID
+    {
+        public int id { get; set; }
+
+    }
+}
---
+++ csharp/ConsoleAppKnjiznica/KnjiznicaWebApp/Models/Knjiga.cs
@@ -0,0 +1,11 @@
+﻿namespace KnjiznicaWebApp.Models
+{
+    public class Knjiga:ID
+    {
+        public string? Naziv { get; set; }
+        public string ImeAutora { get; set; }
+        public string PrezimeAutora { get; set; }
+        public string? Sazetak { get; set; }
+        public int BrStranica { get; set; }
+    }
+}
