78721e6748391d42bea891ab380425f2f2e261bb
Lanaa <lana.jelenic@gmail.com>
2023-10-04 12:15:53 +0200

    dodavanje wwwroota

--- csharp/KnjiznicaApp/KnjiznicaApp/Program.cs
+++ csharp/KnjiznicaApp/KnjiznicaApp/Program.cs
@@ -54,18 +54,18 @@ builder.Services.AddDbContext<KnjiznicaContext>(o =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger(opcije =>
-//    {
-//        opcije.SerializeAsV2 = true;
-//    });
-//    app.UseSwaggerUI(opcije =>
-//    {
-//        opcije.ConfigObject.
-//        AdditionalItems.Add("requestSnippetsEnabled", true);
-//    });
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(opcije =>
+    {
+        opcije.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(opcije =>
+    {
+        opcije.ConfigObject.
+        AdditionalItems.Add("requestSnippetsEnabled", true);
+    });
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
